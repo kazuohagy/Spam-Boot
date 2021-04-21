f564ab4ba20c0e22e96d22bc8f76950b777145e7
Japaranha <kazucalado@gmail.com>
2021-04-21 15:39:09 -0300

    Terminate Cod

--- CodeSpam.py
+++ CodeSpam.py
@@ -1,10 +1,16 @@
-import pyautogui
+import pyautogui, time
+time.sleep(4)
+m = open("Shrek.txt","r")
 pyautogui.keyDown('alt')
 pyautogui.press(['tab'])
 pyautogui.keyUp('alt')
 
-for i in range(100):
+for each_line in m:
+    pyautogui.typewrite(each_line)
+    pyautogui.press('enter')
+
+#for i in range(100):
     #pyautogui.write('Eu sou o mario\nA princesa esta em outro castelo\nIts me Mario ')
     #pyautogui.press('enter')
-    pyautogui.press(['left','right'])
+ #   pyautogui.press(['left','right'])
 
